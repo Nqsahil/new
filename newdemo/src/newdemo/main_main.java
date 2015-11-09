52b3e33ee5d9e8e33d9c44f65650b1674fd25f4a
nqsahil <nq_sahil@outlook.com>
2015-11-09 07:44:37 +0500

    second comment

--- newdemo/src/newdemo/main_main.java
+++ newdemo/src/newdemo/main_main.java
@@ -5,6 +5,9 @@ public class main_main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("heloo");
+int a=20;
+int b=a-5;
+System.out.println("value of b :"+b);
 	}
 
 }
